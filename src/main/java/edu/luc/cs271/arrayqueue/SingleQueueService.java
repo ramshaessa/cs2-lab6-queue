92f8ffc37aaebbb26a28248236bb7d2a58525a92
Ramsha Essa <ressa@luc.edu>
2018-10-31 00:48:05 +0000

    fightfkfkf

--- src/main/java/edu/luc/cs271/arrayqueue/SingleQueueService.java
+++ src/main/java/edu/luc/cs271/arrayqueue/SingleQueueService.java
@@ -9,6 +9,7 @@ public class SingleQueueService {
 
   public static void main(final String[] args) throws InterruptedException {
     // TODO read successive input lines until EOF and try to add them to the queue
+    
 
     // queue for customer names
     final SimpleQueue<String> queue = new FixedArrayQueue<>(5);
@@ -24,8 +25,8 @@ public class SingleQueueService {
                 String current;
 		int remaining;
                 synchronized (lock) {
-                  current = null; // TODO try to take next name from queue
-		  remaining = 0; // TODO determine resulting size of queue
+                  current = queue.peek; // TODO try to take next name from queue
+		  remaining = queue.size // TODO determine resulting size of queue
                 }
                 if (current == null) {
                   System.out.println("no one waiting");
@@ -49,7 +50,7 @@ public class SingleQueueService {
       final String name = input.nextLine();
       boolean result;
       synchronized (lock) {
-        result = false; // TODO try to add this name tothe queue
+        result = queue.offer(name); // TODO try to add this name to the queue
       }
       if (result) {
         System.out.println(name + " has joined the queue");
